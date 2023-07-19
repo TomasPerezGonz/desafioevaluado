fb17c1002c4164e397acaea4b9beddeeaad98f85
Tomas <tomasperezg@gmail.com>
2023-07-19 12:04:36 -0400

    Se arregla problema de creación donde se agregó una , por error en rails g

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-  def home,
+  def home
   end
 
-  def projects,
+  def projects
   end
 
   def contact
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Se definen las rutas para los 3 archivos del proyecto
-  get 'pages/home,'
-  get 'pages/projects,'
+  get 'pages/home'
+  get 'pages/projects'
   get 'pages/contact'
 
   # Se define como raíz home
